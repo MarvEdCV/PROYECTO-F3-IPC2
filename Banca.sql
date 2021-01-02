1f3ffde6079877d107c5931ac553528bac73c9b0
Eduardo Catalán <marvineduardocv12@gmail.com>
2021-01-02 00:22:34 -0600

    script

--- Banca.sql
+++ Banca.sql
@@ -1,14 +1,12 @@
 create database Banca;
 use Banca;
-
+drop database banca;
 create table ADMINISTRADOR(
 Nombre varchar(15) not null,
 contra varchar(20) not null,
 primary key(Nombre)
 );
 insert into ADMINISTRADOR(Nombre,contra) values('admin','admin');
-select * from clienteindividual;
-
 
 create table USUARIOINDIVIDUAL(
 CUI bigint(20) primary key,
@@ -31,17 +29,18 @@ idUsuario int auto_increment primary key,
 contra varchar(20),
 CUI bigint(20),
 idUsuarioemp int,
+deuda decimal(65,3),
+cantidadtarjetas int(1) default 0,
 foreign key (CUI) references USUARIOINDIVIDUAL(CUI),
 foreign key (idUsuarioemp) references USUARIOEMPRESARIAL(idUsuarioemp)
 );
-alter table USUARIO add deuda decimal(65,3);
-DESC USUARIO;
 
 create table CUENTA(
 numerocuenta int auto_increment primary key,
 idUsuario int,
 tipomoneda enum('Q','$'),
 estaActiva enum('si','no'),
+tipocuenta enum('ahorro','monetaria','plazo fijo'),
 foreign key(idUsuario)references USUARIO(idUsuario)
 );
 
@@ -58,7 +57,6 @@ interes int,
 saldo decimal(35,3),
 foreign key(numerocuenta) references CUENTA(numerocuenta)
 );
-
 create table CUENTAPF(
 numerocuenta int primary key,
 interes int,	
@@ -67,8 +65,36 @@ tiempo enum('3','6','12','24'),
 foreign key(numerocuenta) references CUENTA(numerocuenta)
 );
 
+create table TARJETADECREDITO(
+numerotarjeta int auto_increment primary key ,
+idUsuario int,
+marca enum('puntos','cashback'),
+foreign key(idUsuario)references USUARIO(idUsuario)
+);
 
-
+create table TARJETADEPUNTOS(
+numerotarjeta int primary key,
+limite decimal(15,3),
+puntos decimal(35,3) default 0,
+foreign key(numerotarjeta) references TARJETADECREDITO(numerotarjeta)
+);
+create table TARJETADECASHBACK(
+numerotarjeta int primary key,
+limite decimal(15,3),	
+cashback decimal(35,3) default 0,
+foreign key(numerotarjeta) references TARJETADECREDITO(numerotarjeta)
+);
+create table COMPRATARJETA(
+id_compra int auto_increment primary key,
+fechacompra date,
+idUsuario int,
+descripcion varchar(60),
+monto decimal(15,2),
+numerotarjeta int,
+moneda enum('Q','$'),
+foreign key(idUsuario)references USUARIO(idUsuario),
+foreign key(numerotarjeta) references TARJETADECREDITO(numerotarjeta)
+);
 
 
 
